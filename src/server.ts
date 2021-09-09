dcb7ed194e4bbdeae6d91f7221bd50d36900ab65
murilo <murilo.laface@cactvs.com.br>
2021-09-09 11:11:35 -0300

    go back @types/node because conflicts in globals and passed a first test

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
+import { ForecastController } from './controllers/forecast';
 import "./util/module-alias";
 import { Server } from "@overnightjs/core";
-import express from "express";
+import express, { Application } from "express";
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -10,10 +11,20 @@ export class SetupServer extends Server {
   //método responsável por iniciar o servidor
   public init(): void {
     this.setupExpress();
+    this.setupControllers();
   }
   //método responsável por iniciar o setupExpress
   //é como se fosse o middle. se não for um
   private setupExpress(): void {
     this.app.use(express.json());
+  }
+
+  private setupControllers():void {
+    const forecastControllers = new ForecastController();
+    this.addControllers([forecastControllers]);
+  }
+
+  public getApp(): Application{
+    return this.app;
   }
 }
